509b882d7270a545684d1d6746a4344b1fae20c9
gormlabenz <gorm-labenz@hotmail.com>
2022-03-01 10:11:40 +0100

    changed ProductDetail layout

--- src/views/ProductDetail.vue
+++ src/views/ProductDetail.vue
@@ -1,11 +1,18 @@
 <template>
-    <div>
-        <div v-if="product">
+    <div class="my-8">
+        <div class="flex gap-4">
+            <div
+                class="h-96 w-96 rounded-md bg-gray-200"
+                v-for="i in 3"
+                :key="i"
+            ></div>
+        </div>
+        <div class="mt-8" v-if="product">
             <h1 class="text-2xl font-bold">{{ product.name }}</h1>
-            <p v-html="product.description"></p>
+            <p class="mt-4 w-96" v-html="product.description"></p>
             <button
                 @click="commerceStore.addToCart(product)"
-                class="bg-blue-300 py-2 px-4"
+                class="mt-4 rounded-md bg-blue-300 py-2 px-4 font-bold text-white"
             >
                 Add To Card
             </button>
