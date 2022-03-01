01c27152824a4590ad2c7120e100ffaac95e829c
gormlabenz <gorm-labenz@hotmail.com>
2022-03-01 11:12:09 +0100

    added style to select

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -251,6 +251,9 @@ const validate = (form) => {
 input {
     @apply mb-4 block w-full rounded-md border border-gray-200 p-2;
 }
+select {
+    @apply mb-4 block w-full rounded-md border border-gray-200 p-2;
+}
 button {
     margin-left: 100%;
     @apply -translate-x-full rounded-md bg-blue-500 py-2 px-4 font-bold
