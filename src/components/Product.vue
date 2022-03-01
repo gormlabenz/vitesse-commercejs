d7fc89fff7668c36af27cae7ef5d5c9f31fdd9de
gormlabenz <gorm-labenz@hotmail.com>
2022-03-01 10:05:06 +0100

    changed Product layout

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,7 +1,12 @@
 <template>
-    <router-link :to="'/product/' + product.permalink" class="w-64 text-left">
-        <h3 class="text-xl font-bold">{{ product.name }}</h3>
-        <p v-html="product.description"></p>
+    <router-link
+        :to="'/product/' + product.permalink"
+        class="w-64 overflow-hidden rounded-md text-left shadow-md"
+        ><div class="h-48 bg-gray-200"></div>
+        <div class="p-4">
+            <h3 class="text-xl font-bold">{{ product.name }}</h3>
+            <p>{{ product.price.formatted_with_code }}</p>
+        </div>
     </router-link>
 </template>
 <script setup>
